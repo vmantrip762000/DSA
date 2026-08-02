bb75caf4e7ee0e14ad5963d39528d55ba1b1ec6a
Mantri Aditya <159584214+vmantrip762000@users.noreply.github.com>
2026-08-02 12:05:06 -0400

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = {}
+        for c in s:
+            s_dict[c] = 1 + s_dict.get(c, 0)
+        for c in t:
+            if c in s_dict and s_dict[c] > 0:
+                s_dict[c] -= 1
+            else:
+                return False
+        return sum(s_dict.values()) == 0
+        
